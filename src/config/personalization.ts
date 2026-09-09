b42a515b18c2e50fc19491220f96146b89a650ab
Aditya Raikar <adityaraikar5555@gmail.com>
2026-09-09 22:05:48 +0530

    Change password gate secret key

--- src/config/personalization.ts
+++ src/config/personalization.ts
@@ -109,7 +109,7 @@ export const PERSONALIZATION: PersonalizationConfig = {
   creatorRole: "The person who made this little world for you, Sneha Kolvekar ✨",
   creatorPhoto: "/creator/aditya-raikar.jpg",
   recipientPhoto: "/recipient/sneha-kolvekar.png",
-  appPassword: "555",
+  appPassword: "Raikar@123",
   enablePasswordGate: true,
 
   /**
